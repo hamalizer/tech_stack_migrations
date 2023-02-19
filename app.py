1cbd046898ca878cdc434c50ccc20acec35ac627
ham <18280030+hamalizer@users.noreply.github.com>
2023-02-18 19:41:46 -0900

    init

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ st.write(px.scatter_matrix(df, color='year'))
 
 
 
-st.write('It is not a functional application yet. Under construction.') 
+st.write('It is not a functional application yet. Under construction.') https://github.com/hamalizer/tech_stach_migrations
