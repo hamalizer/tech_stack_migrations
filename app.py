ebf992fbb8825eec1f950cf770ac1bfc0d8cbfc8
ham <18280030+hamalizer@users.noreply.github.com>
2023-02-18 19:28:54 -0900

    initial

---
+++ app.py
@@ -0,0 +1,17 @@
+# imports
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+# read dataframe
+df = pd.read_csv('migrations.csv')
+
+st.header('Tech Stack Migrations: All roads lead to Go.')
+
+st.write(px.histogram(df, x='year', color='to'))
+
+st.write(px.scatter_matrix(df, color='year'))
+
+
+
+st.write('It is not a functional application yet. Under construction.') 
