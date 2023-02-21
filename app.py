919b0aa2f7266ea00336c79f1b415cd24529699f
ham <18280030+hamalizer@users.noreply.github.com>
2023-02-20 18:38:32 -0900

    fixed site text

--- app.py
+++ app.py
@@ -23,13 +23,9 @@ st.write(px.histogram(df, x='year', color='to'))
 st.write('An illustration of stack-to-stack changes, colored by-year.')
 st.write(px.scatter(df, x='to', y='from', color='year'))
 
-# a long string
-longstring = 'We can see Golang is a very popular choice for tech companies who have decided to migrate their stack to a new language.<br>By-totals, the total number of migrations to golang is near double when accounting for the fact that TiDB is written in GO'
 #conclusion statement
-st.write(longstring)
-
-
-
+st.write('We can see Golang is a very popular choice for tech companies who have decided to migrate their stack to a new language.')
+st.write('By-totals, the total number of migrations to golang is near double when accounting for the fact that TiDB is written in GO')
 
 
 st.write('Source on Github:[link](https://github.com/hamalizer/tech_stack_migrations)')
